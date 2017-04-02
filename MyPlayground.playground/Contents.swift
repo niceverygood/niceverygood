cc38b54de7ee4fb56989953d02f3ffc4293d1c88
SEUNGSOO HAN <niceverygood@naver.com>
2017-04-02 10:35:47 +0900

    연습중

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var parent = 
+var child =
+
+/*
+while (parent > 0){
+    child = 9
+    while(child > 0){
+        print(parent*child)
+        child = child - 1
+    }
+    parent = parent - 1
+}
+*/
+
+
+func multiple(_parent: Int, _chid:Int) -> Int{
+    return parent*child
+}
